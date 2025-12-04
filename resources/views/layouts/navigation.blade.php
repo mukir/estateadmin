48b9bce90b63bb90644b3435997855cc0af9705b
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-12-04 13:49:52 +0300

    first commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,7 +55,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3 flex items-center justify-between gap-3">
             <a href="{{ route('dashboard') }}" class="flex items-center gap-3">
                 @if (!empty($branding['logo_url']))
-                    <img src="{{ $branding['logo_url'] }}" alt="{{ $branding['platform_name'] ?? config('app.name', 'Dashboard') }}" class="h-12 w-auto max-w-[140px] object-contain">
+                    <img src="{{ $branding['logo_url'] }}" alt="{{ $branding['platform_name'] ?? config('app.name', 'Dashboard') }}" class="h-14 w-auto max-w-[200px] object-contain">
                 @else
                     <x-application-logo class="block h-12 w-12 fill-current text-indigo-600" />
                 @endif
